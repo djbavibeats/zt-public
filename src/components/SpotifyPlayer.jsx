10e56e3a00cf62a9f288a89f49a5045f46059e73
djbavibeats <justin@voltcreative.com>
2024-01-19 18:12:11 -0500

    Switch to Soundcloud

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -55,7 +55,7 @@ export default function SpotifyPlayer(props) {
     <iframe 
     ref={ soundcloud }
     width="100%" 
-    height="100" 
+    height="75" 
     scrolling="no" 
     frameborder="no" 
     allow="autoplay"
